333c52b138a8cebf2c68aac5131690dbeab2d3b7
Sean Oughton <seanoughton@gmail.com>
2018-02-14 19:22:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
-      course.description =
+      course.description = post.css("p").text
     end
   end
 
